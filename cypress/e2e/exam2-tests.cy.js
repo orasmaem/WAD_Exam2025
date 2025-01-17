1dcbf86937fb9e5b645ec76d5ddd67ac6d48f16f
orasmaem <mihkel.orasmae@gmail.com>
2025-01-17 14:15:07 +0200

    Task9

--- cypress/e2e/exam2-tests.cy.js
+++ cypress/e2e/exam2-tests.cy.js
@@ -1,6 +1,11 @@
 /// <reference types = "cypress"/>  
 
 
-
+it('Instructor Page Test', () => {
+   
+    cy.visit('http://localhost:8080/Instructor');
+  
+    cy.get('h1').should('contain', 'Instructor Management Pannel');
+  });
 
 
